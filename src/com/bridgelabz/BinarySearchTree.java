3a14d495187afe368eb6d66a6d591815aa0208bf
Swayam Prakash <swayam265@gmail.com>
2022-09-16 02:25:00 +0530

    [Swayam] : added functionality of creation of bst by adding element

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,50 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+	
+	Node root;
+	
+	public void addNode(int data) {
+		
+		Node node = new Node(data);
+		node.data = data;
+		node.left = null;
+		node.right = null;
+		
+		if(root == null) {
+			root = node;
+			return;
+		}
+		else {
+			Node parent = root;
+			Node current = root;
+			while(current != null) {
+				if(node.data > current.data) {
+					current = current.right;
+				}
+				else {
+					current = current.left;
+				}
+			}
+			if(node.data > parent.data) {
+				parent.right = node;
+			}
+			else {
+				parent.left = node;
+			}
+		}
+	}
+	  
+	// method to print tree
+	
+	public void printTree(Node node) {
+		if(node.left != null) {
+			printTree(node.left);
+		}
+		System.out.println(node.data+" ");
+		if(node.right != null) {
+			printTree(node.right);
+		}
+	}
+
+}
