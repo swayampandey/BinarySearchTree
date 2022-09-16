62e7fed3943a1d0a95e38fa8af6bc4d1251d954f
Swayam Prakash <swayam265@gmail.com>
2022-09-16 13:50:19 +0530

    [Swayam] : added functionality of creation of bst diagram according to the given diagram

--- src/com/bridgelabz/BstMain.java
+++ src/com/bridgelabz/BstMain.java
@@ -1,29 +1,34 @@
 package com.bridgelabz;
 
 public class BstMain {
-
+	
 	public static void main(String[] args) {
 		
-		BinarySearchTree binaryTree = new BinarySearchTree();
-		System.out.println("\nWelcome to Binary Search Tree\n");
-		binaryTree.addNode(56);
-		binaryTree.addNode(30);
-		binaryTree.addNode(70);
+		System.out.println("Welcome to Binary search tree");
+		
+			BinarySearchTree binaryTree = new BinarySearchTree();
+			
+			binaryTree.addNode(56);
+			binaryTree.addNode(30);
+			binaryTree.addNode(70);
+			
 		System.out.println("\nBinary Search Tree After Adding Elements\n ");
-		binaryTree.printTree(binaryTree.root);
-		binaryTree.addNode(22);
-		binaryTree.addNode(40);
-		binaryTree.addNode(11);
-		binaryTree.addNode(3);
-		binaryTree.addNode(16);
-		binaryTree.addNode(60);
-		binaryTree.addNode(95);
-		binaryTree.addNode(65);
-		binaryTree.addNode(63);
-		binaryTree.addNode(67);
+		
+			binaryTree.printTree(binaryTree.root);
+			binaryTree.addNode(22);
+			binaryTree.addNode(40);
+			binaryTree.addNode(11);
+			binaryTree.addNode(3);
+			binaryTree.addNode(16);
+			binaryTree.addNode(60);
+			binaryTree.addNode(95);
+			binaryTree.addNode(65);
+			binaryTree.addNode(63);
+			binaryTree.addNode(67);
+			
 		System.out.println("\nAfter Adding Remaining Elements into Tree\n");
+		
 		binaryTree.printTree(binaryTree.root);
-
 	}
 
 }
